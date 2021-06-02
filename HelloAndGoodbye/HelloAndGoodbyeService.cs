08ca699349f3b58b7b325aa213cb7ab7ed6b580a
sdelrosario <sdelrosario@PHUTISOF001>
2021-06-02 10:15:06 -0400

    Add project files.

---
+++ HelloAndGoodbye/HelloAndGoodbyeService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace HelloAndGoodbye
+{
+    partial class HelloAndGoodbyeService : ServiceBase
+    {
+        #region Declaration of instances
+        private int eventId = 1;
+        Timer timer;
+        EventLog eventLog;
+        #endregion
+
+        public HelloAndGoodbyeService()
+        {
+            InitializeComponent();
+
+            eventLog = new EventLog();
+            if (!EventLog.SourceExists("HelloAndGoodbyeService"))
+            {
+                EventLog.CreateEventSource("HelloAndGoodbyeService", "HelloAndGoodbyeLog");
+            }
+            eventLog.Source = "HelloAndGoodbyeService";
+            eventLog.Log = "HelloAndGoodbyeLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            try
+            {
+                eventLog.WriteEntry("Good Morning! Starting the day...", EventLogEntryType.Information);
+                timer = new Timer();
+                timer.Interval = 10000; //10 seconds
+                timer.Elapsed += new ElapsedEventHandler(this.OnTimedEvent);
+                timer.Start();
+            }
+
+            catch (Exception ex)
+            {
+                eventLog.WriteEntry($"Error: {ex.Message}; StackTrace: {ex.StackTrace};", EventLogEntryType.Error);
+            }
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var time = DateTime.Now;
+
+                eventLog.WriteEntry("Log said hello! at: " + time.ToString(), EventLogEntryType.Information, eventId++);
+            }
+
+            catch (Exception ex)
+            {
+                eventLog.WriteEntry($"Error: {ex.Message}; StackTrace: {ex.StackTrace};", EventLogEntryType.Error);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            eventLog.WriteEntry("Goodbye! Stopping the day...", EventLogEntryType.Warning);
+            timer.Stop();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.IContainer components = new System.ComponentModel.Container();
+            this.ServiceName = "HelloAndGoodbye";
+        }
+    }
+}
---
+++ HelloAndGoodbye/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace HelloAndGoodbye
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HelloAndGoodbyeService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
